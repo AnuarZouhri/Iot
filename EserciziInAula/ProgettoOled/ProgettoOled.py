9bc2611bc69635953c7095923cd1069d2c0043db
Annamaria Scermino <a.scermino7@studenti.unisa.it>
2025-04-13 21:49:48 +0200

    Update ProgettoOled.py

--- EserciziInAula/ProgettoOled/ProgettoOled.py
+++ EserciziInAula/ProgettoOled/ProgettoOled.py
@@ -1,8 +1,7 @@
 from machine import ADC, Pin, PWM
 import time
+import ssd1306
 
-
-pwm2 = PWM(Pin(27), freq=50) '<- servo motore'
 
 class LED:
     def __init__(self, sig_pin, flag=True, freq=5000):
@@ -50,22 +49,43 @@ def set_angle(angle):
     duty_min = 35
     duty_max = 133
     pwm2.duty(int(duty_min + (angle/180)*(duty_max-duty_min)))
+    
+    
+WIDTH = 128
+HEIGHT = 64
+SCL_PIN = 22
+SDA_PIN = 21
+
+i2c = I2C(0, scl=Pin(SCL_PIN), sda=Pin(SDA_PIN))
+display = ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
 
 ldr = LDR(34,0,180)
 led = LED(14)
+pwm2 = PWM(Pin(27), freq=50) '<- servo motore'
 led.ledOff()
+set_angle(0)
 
+oldValue=ldr.value()
+display.text("Value: ".format(oldValue), 11, 56)
+display.show()
+time.sleep_ms(1000)
 while True:
-    value = ldr.value()
-    if value > 60:
-        led.ledOn()
+    newValue = ldr.value()
+    if newValue != oldValue:
+        display.text("Value: ".format(newValue), 11, 56)
+        display.show()
+        time.sleep_ms(1000)
+    if newValue > 60:
+        led.ledOff()
         set_angle(180)
-    led.ledOff()
-    set_angle(0)
+    else
+        led.ledOn()
+        set_angle(0)
+    oldValue=newValue
         
     
-    
+
 
 
 
