1716020b61cdf6d9d760eb1115c08d4593fca02f
Gerardo Selce <gerardoselce28@gmail.com>
2025-05-14 16:24:44 +0200

    main aggiornato

--- Progetto/Classi/main.py
+++ Progetto/Classi/main.py
@@ -1,14 +1,15 @@
 import os
+from KeyPad import KeyPad
 
 """ Stati principali """
-STATO_CONFIGURAZIONE_SISTEMA = 0
-STATO_CONNESSIONE = 1
-STATO_VISTA_MENU = 2
-STATO_CAMBIO_CONFIGURAZIONE = 3
-STATO_INSERIMENTO_PIN = 4
-STATO_SBLOCCATO = 5
-STATO_BLOCCATO = 6
-STATO_RICONNESSIONE = 7
+STATO_CONFIGURAZIONE_PIN = 0
+STATO_CONFIGURAZIONE_WIFI = 1
+STATO_CONNESSIONE = 2
+STATO_VISTA_MENU = 3
+STATO_CAMBIO_CONFIGURAZIONE = 4
+STATO_INSERIMENTO_PIN = 5
+STATO_SBLOCCATO = 6
+STATO_BLOCCATO = 7
 
 """ Stato allarme """
 STATO_ALLARME = 8
@@ -20,30 +21,48 @@ file_nome_wifi = "wifi.txt"
 file_pin = "pin.txt"
 
 
+
+""" Definizione di sensori e attuatori """
+pad = KeyPad(2,4,5,18,19,21,22,23)  # Tastierino numerico
+
+
 """ Definizione stato corrente """
 stato = ""
 
 
 """ Inizializzazione dello stato """
 if not file_pin in os.listdir():
-    stato = STATO_CONFIGURAZIONE_SISTEMA
+    stato = STATO_CONFIGURAZIONE_PIN
 else:
     stato = STATO_CONNESSIONE
     
 
 while True:
     
-    #
-    # CONTROLLO DELLO STATO DI RICONNESSIONE
-    #
-    #
+    if stato == STATO_CONFIGURAZIONE_PIN:
+        print('Inserire pin!!')
+        password = ''
     
-    if stato == STATO_CONFIGURAZIONE_SISTEMA:
+        for i in range(4):
+            key = pad.lettura()
+            while key == None:
+                key = pad.lettura()
+            password = password + key
+
+        with open('pin.txt', 'w') as f:
+            f.write(password)
+        
+        #
+        # AGGIORNAMENTO DELLO STATO
+        #
+        stato = STATO_CONFIGURAZIONE_WIFI
+        
+    elif stato == STATO_CONFIGURAZIONE_WIFI:
         #
         # Istruzioni
         #
         # stato = NUOVO_STATO
-    elif stato == STATO_CONNESSIONE:
+    elif stato = STATO_CONNESSIONE:
         #
         # Istruzioni
         #
@@ -73,17 +92,16 @@ while True:
         # Istruzioni
         #
         # stato = NUOVO_STATO
-    elif stato == STATO_RICONNESSIONE:
-        #
-        # Istruzioni
-        #
-        # stato = NUOVO_STATO
     elif stato == STATO_ALLARME:
         #
         # Istruzioni
         #
         # stato = NUOVO_STATO
     
+    #
+    # Verifica lo stato di connessione
+    #
+    #
 
 
     
