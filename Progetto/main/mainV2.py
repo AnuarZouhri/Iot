907c3cca79f9c629fa90f54a873b27597c03c1f8
Annamaria Scermino <a.scermino7@studenti.unisa.it>
2025-05-30 21:02:22 +0200

    versioni del main

---
+++ Progetto/main/mainV2.py
@@ -0,0 +1,365 @@
+from time import sleep, localtime, ticks_ms, ticks_diff
+from KeyPad import KeyPad
+from OledClass import Oled
+from WiFiClass import WiFi
+from dht22 import DHT22
+from UltrasonicSensor import HCSR04
+from Handler import SensorHandler
+from MQTTClass import MQTT
+from PasswordClass import Password
+from servoMotore import ServoMotor
+from mutex import Mutex
+from machine import Pin
+from buzzer_class import BUZZER
+from MessageMaker import MM
+from machine import disable_irq, enable_irq
+
+
+def closeDoor(pin):
+    global last_press_time, stato#, openDoor
+    current_time = ticks_ms()
+    if ticks_diff(current_time, last_press_time) < 200:
+        return
+    last_press_time = current_time
+    
+    if stato == STATO_SBLOCCATO:
+        sv.closeDoor()
+        mutex.lock()
+        openDoor = False
+        stato = STATO_VISTA_MENU
+        #oled.writeLogo(logo_unlock)
+        #sleep(0.8)
+        
+def stopBuzzer(pin):
+    global stato, last_press_time_buzzer, flagBuzzer
+    current_time=ticks_ms()
+    if ticks_diff(current_time, last_press_time) < 200:
+        return
+    last_press_time_buzzer = current_time
+    
+    if stato==STATO_ALLARME:
+        flagBuzzer=True
+        
+def checkTempHum(values):
+    global stato
+    temp=values['Temperature']
+    hum=values['Humidity']
+    if temp>50 or hum>70:
+        stato=STATO_ALLARME
+
+def checkDistance():
+    global stato, standardDistance
+    if hcsr04.distanceCm()<standardDistance:
+        stato=STATO_ALLARME
+        
+    
+        
+
+"""Callback handler"""
+
+def sub_callback_handler(topic,msg):
+        global stato, SUB_TOPICS, pin, mqtt
+        print(topic)
+        print(SUB_TOPICS[0])
+        if topic == SUB_TOPICS[0]:
+            #
+            # AGGIORNAMENTO DELLO STATO
+            #
+            stato = STATO_SBLOCCATO
+            
+        if topic == SUB_TOPICS[1]:
+            #
+            # AGGIORNAMENTO DELLO STATO
+            #
+            stato = STATO_BLOCCATO
+            
+        if topic == SUB_TOPICS[5]:
+            password = msg.decode('utf-8')
+            newmsg = ''
+            mm = MM()
+            
+            if pin.checkPassword(password):
+                stato = STATO_SBLOCCATO
+                newmsg = mm.correctPinMsg()
+            else:
+                newmsg = mm.wrongPinMsg()
+            
+            #mqtt.publish(SUB_TOPICS[6], newmsg)
+                
+
+
+""" Stati principali """
+STATO_CONFIGURAZIONE_PIN = 0
+STATO_CONFIGURAZIONE_WIFI = 1
+STATO_CONNESSIONE = 2
+STATO_VISTA_MENU = 3
+STATO_CAMBIO_CONFIGURAZIONE = 4
+STATO_INSERIMENTO_PIN = 5
+STATO_SBLOCCATO = 6
+
+""" Stato allarme """
+STATO_ALLARME = 8
+
+
+""" Definizione di sensori e attuatori """
+pad = KeyPad(15,4,5,18,19,14,12,33)  # Tastierino numerico
+oled = Oled()   # Oled Pin 22 e Pin 21
+dht22 = DHT22(23)
+hcsr04 = HCSR04(32,34)
+sv=ServoMotor(26)
+mutex = Mutex(16, 2, 25)
+buzzer = BUZZER(17)
+btnBuzzer=Pin(13, Pin.PULL_DOWN)
+btnBuzzer.irq(trigger=Pin.IRQ_RISING, handler=stopBuzzer)
+last_press_time_buzzer=0
+
+mutex.lock()
+button = Pin(0, Pin.IN, Pin.PULL_DOWN)
+button.irq(trigger=Pin.IRQ_RISING, handler=closeDoor)
+last_press_time = 0
+
+"""Altri oggetti utili"""
+wifi = WiFi('Galaxy A5173BB', 'aaaaaaab')
+mqtt = MQTT(sub_callback_handler)
+handler = SensorHandler(dht22,hcsr04)
+pin = Password()
+values = []
+flagCambioConfigurazione = False
+flagBuzzer= False
+standardDistance=17
+openDoor=False
+
+
+
+""" Definizione stato corrente """
+stato = -1
+was_connected_WiFi = 0
+was_connected_MQTT = 0
+SUB_TOPICS = mqtt.getSUB_TOPICS()
+
+
+""" Inizializzazione dello stato """
+if not pin.fileExists():
+    stato = STATO_CONFIGURAZIONE_PIN
+else:
+    stato = STATO_CONFIGURAZIONE_WIFI
+
+oled.writeLogo()
+oled.show()
+sleep(1)
+while True:
+    values = handler.read()
+    print(values)
+    checkTempHum(values)
+    checkDistance()
+    
+    was_connected_MQTT = mqtt.checkAndRead_msg(wifi, was_connected_MQTT, values)
+    
+    if stato == STATO_CONFIGURAZIONE_PIN:
+        sv.closeDoor()
+        
+        print('Inserire pin!!')
+        pos = oled.write(1, 1, 0, 'Per registrarti,\n')
+        pos = oled.write(pos[0], pos[1], 0,'inserire il pin!\n', clean=False)
+        oled.show()
+        print(pos)
+        
+        password = pad.letturaPin(oled, pos)
+
+        pin.write(password)
+        
+        
+        oled.write(1,1,0,'Pin inserito!',clean=True)
+        oled.show()
+        
+        #
+        # AGGIORNAMENTO DELLO STATO
+        #
+        stato = STATO_CONFIGURAZIONE_WIFI
+        
+        
+        
+        
+    elif stato == STATO_CONFIGURAZIONE_WIFI:
+        oled.write(1,1,0,'Benvenut*!')
+        oled.show()
+        sleep(1)
+        
+        pos = oled.write(1, 1, 0, 'Connessione al\n')
+        pos = oled.write(pos[0], pos[1], 0, 'wifi in corso...', clean=False)
+        oled.show()
+        
+        wifi.connectionWiFi()
+        
+        if wifi.isconnected():
+            pos = oled.write(1, 1, 0, 'Connessione\nriuscita!')
+            oled.show()
+            stato = STATO_CONNESSIONE
+            was_connected_WiFi = 1
+        else:
+            pos = oled.write(1, 1, 0, 'Connessione\nnon riuscita..')
+            oled.show()
+            stato = STATO_VISTA_MENU
+            print('Aggiorno stato su vista menu')
+            was_connected_WiFi = 0
+        sleep(1)
+         
+        
+    elif stato == STATO_CONNESSIONE:
+        pos = oled.write(1, 1, 0, 'Connessione MQTT...')
+        oled.show()
+        try:
+            mqtt.connect()
+            mqtt.subscribes()
+            oled.write(1,1,0,'Connessione MQTT riuscita!')
+            oled.show()
+            was_connected_MQTT = 1
+            sleep(0.1)
+        except OSError as e:
+            oled.write(1,1,0,'Connessione MQTT non riuscita!')
+            oled.show()
+            sleep(0.1)
+            print('Connessione MQTT non riuscita')
+  
+        
+        #
+        # AGGIORNAMENTO DELLO STATO
+        #
+        stato = STATO_VISTA_MENU
+        
+        
+        
+    elif stato == STATO_VISTA_MENU:
+        timeout = 3500  # millisecondi (es. 5 secondi)
+        timestamp = localtime()
+        stringa = str(values['Temperature'])+'C'+'|'+str(values['Humidity'])+'%|'+str(timestamp[3])+':'+str(timestamp[4])+'\n'
+        pos = oled.write(1,1,0,'1-Apri Porta\n')
+        pos = oled.write(pos[0], pos[1], 0, '2-Cambia config\n', clean=False)
+        oled.write(pos[0], pos[1], 0, stringa,clean=False)
+        oled.show()
+        
+        key = pad.lettura()
+        start_time = ticks_ms()
+        
+        while key == None and ticks_diff(ticks_ms(), start_time) < timeout and stato != STATO_ALLARME:
+            was_connected_MQTT = mqtt.checkAndRead_msg(wifi, was_connected_MQTT, values)
+            key = pad.lettura()
+            checkDistance()
+        print('Hai premuto',key)
+        
+        
+        #
+        # AGGIORNAMENTO DELLO STATO
+        #
+        if key == '1':
+            stato = STATO_INSERIMENTO_PIN
+        elif key == '2':
+            stato = STATO_CAMBIO_CONFIGURAZIONE
+        elif not wifi.isconnected() and key==None:
+            oled.write(1, 1, 0, 'Nessuna connes\nsione WiFi')
+            oled.show()
+            stato = STATO_CONFIGURAZIONE_WIFI
+            sleep(0.5)
+        
+      
+        sleep(0.3)
+        
+        
+        
+   
+   
+        
+    elif stato == STATO_CAMBIO_CONFIGURAZIONE:
+        pos = oled.write(1, 1, 0, 'Hai deciso di\nmodificare il\npin.')
+        sleep(0.5)
+        
+        flagCambioConfigurazione = True
+        
+        #
+        # AGGIORNAMENTO DELLO STATO
+        #
+        stato = STATO_INSERIMENTO_PIN
+        
+
+    elif stato == STATO_INSERIMENTO_PIN:
+        password = ''
+        flag = False
+        cont = 0
+        while not pin.checkPassword(password) and cont<3:
+            print(pos)
+            pos = oled.write(1,1,0,'Inserire il pin corrente!\n')
+            oled.show()
+            
+            password = pad.letturaPin(oled, pos)
+            
+            cont = cont + 1
+            if pin.checkPassword(password):
+                print('Pin corretto!')
+                flag = True
+                oled.write(1, 1, 0, 'Pin inserito\ncorrettamente!\n:)')
+                oled.show()
+                sleep(0.5)
+            else:
+                print('Pin errato..')
+                pos = oled.write(1, 1, 0, 'Pin errato.\n'+str(3-cont)+' tentativi\nrimanenti.\n')
+                oled.show()
+                password = ''
+                sleep(2)
+        
+        #
+        # AGGIORNAMENTO DELLO STATO
+        #
+        if cont == 3 and not flag:
+            stato = STATO_ALLARME
+        elif pin.checkPassword(password):
+            if flagCambioConfigurazione:
+                stato = STATO_CONFIGURAZIONE_PIN
+                flagCambioConfigurazione = False
+            else:
+                stato = STATO_SBLOCCATO
+            
+        
+    elif stato == STATO_SBLOCCATO:
+        #oled.writeLogo(logo_unlock)
+        #sleep(1)
+        openDoor=True
+        mutex.unlock()
+        sv.openDoor()
+        
+        oled.write(1, 1, 0, 'Porta aperta!')
+        oled.show()
+        
+        #
+        # AGGIORNAMENTO DELLO STATO
+        #
+        
+
+    elif stato == STATO_ALLARME:
+        pos=oled.write(1, 1, 0, 'Allarme\n')
+        oled.show()
+        while not flagBuzzer:
+            mutex.alarm()
+            buzzer.play([330], 1000, 512)
+        flagBuzzer=False
+        pos = oled.write(pos[0], pos[1], 0, 'Inserisci il pin\n', clean=False)
+        oled.show()
+        password = pad.letturaPin(oled, pos)
+        if pin.checkPassword(password):
+            stato=STATO_VISTA_MENU
+        else:
+            oled.write(1, 1, 0, 'Pin errato')
+            oled.show()
+        
+        #
+        # Istruzioni
+        #
+        # stato = NUOVO_STATO
+    
+    
+    
+    # Disattiva l'IRQ del pin echo
+    
+    
+    sleep(0.3)
+
+
---
+++ Progetto/main/mainV5.py
@@ -0,0 +1,386 @@
+from time import sleep, localtime, ticks_ms, ticks_diff
+from machine import Pin
+from KeyPad import KeyPad
+from OledClass import Oled
+from WiFiClass import WiFi
+from dht22 import DHT22
+from UltrasonicSensor import HCSR04
+from Handler import SensorHandler
+from MQTTClass import MQTT
+from PasswordClass import Password
+from servoMotore import ServoMotor
+from mutex import Mutex
+from buzzer_class import BUZZER
+from MessageMaker import MM
+import ujson
+
+
+def closeDoor(pin):
+    global last_press_time, stato, openDoor
+    
+    current_time = ticks_ms()
+    if ticks_diff(current_time, last_press_time) < 200:
+        return
+    last_press_time = current_time
+    
+    if stato == STATO_SBLOCCATO:
+        sv.closeDoor()
+        mutex.lock()
+        oled.writeLogo()
+        oled.show()
+        sleep(0.75)
+        openDoor = False
+        mqtt.publish(SUB_TOPICS[0], mm.statusDoor(0))
+        stato = STATO_VISTA_MENU
+        
+        
+def stopBuzzer(pin):
+    global stato, last_press_time_buzzer, flagBuzzer
+    
+    current_time=ticks_ms()
+    if ticks_diff(current_time, last_press_time) < 200:
+        return
+    last_press_time_buzzer = current_time
+    
+    if stato==STATO_ALLARME:
+        flagBuzzer=True
+        
+def checkTempHum(values):
+    global stato, tempAllarm, humAllarm
+    temp=values['Temperature']
+    hum=values['Humidity']
+    if temp>50:
+        tempAllarm=True
+        stato=STATO_ALLARME
+    elif hum>70:
+        humAllarm=True
+        stato=STATO_ALLARME
+
+def checkDistance():
+    global stato, standardDistance, distanceAllarm
+    d=hcsr04.distanceCm()
+    if d<standardDistance and stato!=STATO_SBLOCCATO:
+        distanceAllarm=True
+        stato=STATO_ALLARME
+        
+    
+        
+
+"""Callback handler"""
+
+def sub_callback_handler(topic,msg):
+        global stato, SUB_TOPICS, pin, mqtt, openDoor, flagBuzzer, pinRemote, pinWrong
+        print('topic ', topic)
+        if topic == SUB_TOPICS[0]:
+            message=ujson.loads(msg)
+            if 'wrong' in message and message['wrong']==1:
+                stato = STATO_SBLOCCATO
+        elif topic == SUB_TOPICS[1]:
+            sv.closeDoor()
+            mutex.lock()
+            oled.writeLogo()
+            oled.show()
+            sleep(0.75)
+            openDoor = False
+            mqtt.publish(SUB_TOPICS[0], mm.statusDoor(0))
+            stato = STATO_VISTA_MENU
+        elif topic == SUB_TOPICS[5]:
+            password = msg.decode('utf-8')
+            if pinRemote and pin.checkPassword(password):
+                stato=STATO_VISTA_MENU
+            elif pinRemote:
+                pinWrong=True
+            if pin.checkPassword(password):
+                stato = STATO_SBLOCCATO
+            else:
+                mqtt.publish(SUB_TOPICS[7], mm.wrongPinMsg())
+        elif topic == SUB_TOPICS[10]:
+            mqtt.publish(SUB_TOPICS[11], mm.correctPinMsg())
+        elif topic == SUB_TOPICS[12]:
+            print('STOP BUZZER')
+            if stato==STATO_ALLARME:
+                flagBuzzer=True
+                pinRemote=True
+            
+                
+
+""" Stati principali """
+STATO_CONFIGURAZIONE_PIN = 0
+STATO_CONFIGURAZIONE_WIFI = 1
+STATO_CONNESSIONE = 2
+STATO_VISTA_MENU = 3
+STATO_CAMBIO_CONFIGURAZIONE = 4
+STATO_INSERIMENTO_PIN = 5
+STATO_SBLOCCATO = 6
+
+""" Stato allarme """
+STATO_ALLARME = 8
+
+
+""" Definizione di sensori e attuatori """
+pad = KeyPad(15,4,5,18,19,14,12,33)  # Tastierino numerico
+oled = Oled()   # Oled Pin 22 e Pin 21
+dht22 = DHT22(23)
+hcsr04 = HCSR04(32,34)
+sv=ServoMotor(26)
+mutex = Mutex(16, 2, 25)
+buzzer = BUZZER(17)
+btnBuzzer=Pin(13, Pin.PULL_DOWN)
+button = Pin(0, Pin.IN, Pin.PULL_DOWN)
+mm = MM()
+
+btnBuzzer.irq(trigger=Pin.IRQ_RISING, handler=stopBuzzer)
+button.irq(trigger=Pin.IRQ_RISING, handler=closeDoor)
+
+
+"""Altri oggetti utili"""
+wifi = WiFi('Galaxy A5173BB', 'aaaaaaab')
+mqtt = MQTT(sub_callback_handler)
+handler = SensorHandler(dht22,hcsr04)
+pin = Password()
+values = []
+flagCambioConfigurazione = False
+flagBuzzer= False
+openDoor=False
+firstRegistration=True
+tempAllarm=False
+humAllarm=False
+distanceAllarm=False
+wrongPinAllarm=False
+pinRemote=False
+pinWrong=False
+last_press_time_buzzer=0
+last_press_time = 0
+standardDistance=10
+i=1
+
+
+
+""" Definizione stato corrente """
+stato = -1
+was_connected_WiFi = 0
+was_connected_MQTT = 0
+SUB_TOPICS = mqtt.getSUB_TOPICS()
+
+
+
+""" Inizializzazione dello stato """
+if not pin.fileExists():
+    stato = STATO_CONFIGURAZIONE_PIN
+else:
+    stato = STATO_CONFIGURAZIONE_WIFI
+
+
+""" Stato iniziale """
+mutex.lock()
+sv.closeDoor()
+oled.writeLogo()
+oled.show()
+sleep(0.75)
+
+
+while True:
+    values = handler.read()
+    checkTempHum(values)
+    checkDistance()
+    
+    was_connected_MQTT = mqtt.checkAndRead_msg(wifi, was_connected_MQTT, values)
+    
+    if stato == STATO_CONFIGURAZIONE_PIN:
+        sv.closeDoor()
+        
+        if not pin.fileExists():
+            pos = oled.write(1, 1, 0, 'Per registrarti,\n')
+            pos = oled.write(pos[0], pos[1], 0,'inserire il pin!\n', clean=False)
+            oled.show()
+        else:
+            oled.write(1, 1, 0, 'Inserisci il\nnuovo pin:\n')
+            oled.show()
+        
+        password = pad.letturaPin(oled, pos)
+        pin.write(password)
+        oled.write(1,1,0,'Pin inserito!',clean=True)
+        oled.show()
+        
+        if wifi.isconnected():
+            stato = STATO_VISTA_MENU
+        else:
+            stato=STATO_CONFIGURAZIONE_WIFI
+
+    elif stato == STATO_CONFIGURAZIONE_WIFI:
+        if firstRegistration:
+            oled.write(1,1,0,'\n\n   Benvenut*!')
+            oled.show()
+            sleep(1)
+            firstRegistration=False
+        
+        pos = oled.write(1, 1, 0, '\n\n  Connessione al')
+        pos = oled.write(pos[0], pos[1], 0, 'wifi in corso...', clean=False)
+        oled.show()
+        sleep(0.6)
+        
+        wifi.connectionWiFi()
+        
+        if wifi.isconnected():
+            pos = oled.write(1, 1, 0, 'Connessione\nriuscita!')
+            oled.show()
+            stato = STATO_CONNESSIONE
+            was_connected_WiFi = 1
+            i=1
+        else:
+            pos = oled.write(1, 1, 0, 'Connessione\nnon riuscita..')
+            oled.show()
+            stato = STATO_VISTA_MENU
+            print('Aggiorno stato su vista menu')
+            was_connected_WiFi = 0
+        sleep(1)
+         
+        
+    elif stato == STATO_CONNESSIONE:
+        oled.write(1,1,0,'Connessione\nMQTT...')
+        oled.show()
+        sleep(0.5)
+        try:
+            mqtt.connect()
+            mqtt.subscribes()
+            mqtt.publish(SUB_TOPICS[11], mm.correctPinMsg())
+            mqtt.publish(SUB_TOPICS[0], mm.statusDoor(0))
+            was_connected_MQTT = 1
+            oled.write(1,1,0,'Connessione MQTT riuscita!')
+            oled.show()
+            sleep(0.2)
+        except OSError as e:
+            oled.write(1,1,0,'Connessione MQTT non riuscita')
+            oled.show()
+            sleep(0.2)
+        stato = STATO_VISTA_MENU
+        
+        
+        
+    elif stato == STATO_VISTA_MENU:
+        pos = oled.write(1,1,0,'1-Apri Porta\n')
+        pos = oled.write(pos[0], pos[1], 0, '2-Cambio pin\n\n', clean=False)
+        timestamp = localtime()
+        #'\n' + str(timestamp[2]) + '/' + str(timestamp[1]) + '/' + str(timestamp[0]) + ' ' 
+         #           + str(timestamp[3]) + ':' + str(timestamp[4]) + '\n' +
+        stringa =(
+                   'Temp: ' + str(values['Temperature']) + 'C\n' +
+                    'Umid: ' + str(values['Humidity']) + '%\n'
+                   )
+        oled.write(pos[0], pos[1], 0, stringa,clean=False)
+        oled.show()
+        
+        key = pad.lettura()
+        
+        start_time = ticks_ms()
+        timeout = 3500
+        while key == None and ticks_diff(ticks_ms(), start_time) < timeout and stato != STATO_ALLARME:
+            was_connected_MQTT = mqtt.checkAndRead_msg(wifi, was_connected_MQTT, values)
+            key = pad.lettura()
+            checkDistance()
+       
+        if key == '1':
+            stato = STATO_INSERIMENTO_PIN
+        elif key == '2':
+            stato = STATO_CAMBIO_CONFIGURAZIONE
+        elif not wifi.isconnected() and key==None and i%2==0:
+            stato = STATO_CONFIGURAZIONE_WIFI
+            
+        i=i+1
+
+        
+    elif stato == STATO_CAMBIO_CONFIGURAZIONE:
+        flagCambioConfigurazione = True
+        stato = STATO_INSERIMENTO_PIN
+        
+
+    elif stato == STATO_INSERIMENTO_PIN:
+        password = ''
+        flag = False
+        cont = 0
+        while not pin.checkPassword(password) and cont<3:
+            pos = oled.write(1,1,0,'Inserire il pin corrente:\n')
+            oled.show()
+            
+            password = pad.letturaPin(oled, pos)
+            
+            cont = cont + 1
+            if pin.checkPassword(password):
+                flag = True
+                oled.write(1, 1, 0, 'Pin corretto:)')
+                oled.show()
+                sleep(0.5)
+            elif cont<3:
+                if cont==1:
+                    pos = oled.write(1, 1, 0, 'Pin errato.\n'+'2 tentativi\nrimanenti\n')
+                else:
+                    pos = oled.write(1, 1, 0, 'Pin errato.\n'+'Ultimo tentativo\n')
+                oled.show()
+                password = ''
+                sleep(2)
+        
+        if cont == 3 and not flag:
+            stato = STATO_ALLARME
+            wrongPinAllarm=True
+        elif pin.checkPassword(password):
+            if flagCambioConfigurazione:
+                stato = STATO_CONFIGURAZIONE_PIN
+                flagCambioConfigurazione = False
+            else:
+                stato = STATO_SBLOCCATO
+            
+    elif stato == STATO_SBLOCCATO:
+        if openDoor==False:
+            mqtt.publish(SUB_TOPICS[0], mm.statusDoor(1))
+            oled.writeLogoUnlock()
+            oled.show()
+            sleep(0.75)
+        openDoor=True
+        mutex.unlock()
+        sv.openDoor()
+        
+        oled.write(1, 1, 0, 'Porta aperta!')
+        oled.show()
+        
+
+    elif stato == STATO_ALLARME:
+        if tempAllarm:
+            oled.write(1, 1, 0, 'ALLARME\nSupero temperatura')
+        elif humAllarm:
+            oled.write(1, 1, 0, 'ALLARME\nSupero umidità')
+        elif distanceAllarm:
+            oled.write(1, 1, 0, 'ALLARME\nIntrusione')
+            mqtt.publish(SUB_TOPICS[7], mm.correctPinMsg())
+        elif wrongPinAllarm:
+            oled.write(1, 1, 0, 'ALLARME\nPin errato')
+            mqtt.publish(SUB_TOPICS[9], mm.correctPinMsg())
+        oled.show()
+        while not flagBuzzer:
+            mutex.alarm()
+            buzzer.play([330], 1000, 512)
+            was_connected_MQTT = mqtt.checkAndRead_msg(wifi, was_connected_MQTT, values)
+        flagBuzzer=False
+        oled.write(1, 1, 0, 'Inserisci il pin\n')
+        oled.show()
+        if not pinRemote:
+            password = pad.letturaPin(oled, pos)
+            if pin.checkPassword(password):
+                stato=STATO_VISTA_MENU
+                tempAllarm=False
+                humAllarm=False
+                distanceAllarm=False
+                wrongPinAllarm=False
+                mqtt.publish(SUB_TOPICS[8], mm.correctPinMsg())
+                mutex.lock()
+            else:
+                oled.write(1, 1, 0, 'Pin errato')
+                oled.show()
+        else:
+            while not pinWrong:
+                was_connected_MQTT = mqtt.checkAndRead_msg(wifi, was_connected_MQTT, values)
+        pinWrong=False
+        pinRemote=False
+        print('dopo')
+        
+
+
