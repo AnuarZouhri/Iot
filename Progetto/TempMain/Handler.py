b35b48d6f293bcdf7f935a4a11db43f460922b09
Gerardo Selce <gerardoselce28@gmail.com>
2025-05-31 16:07:34 +0200

    handler modificato

--- Progetto/TempMain/Handler.py
+++ Progetto/TempMain/Handler.py
@@ -1,4 +1,5 @@
 from dht22 import DHT22
+import os
 
 """ Classe create per ottenere i valori letti dal sensore di umidità e temperatura """
 class SensorHandler:
@@ -7,9 +8,34 @@ class SensorHandler:
     def __init__(self,dht22, hcsr04, sogliaTemp=50.0, sogliaHum=70.0, sogliaDis=10.0):
         self.dht22 = dht22
         self.hcsr04 = hcsr04
-        self.sogliaTemp = sogliaTemp
-        self.sogliaHum = sogliaHum
-        self.sogliaDis = sogliaDis
+        self.sogliaTemp = ''
+        self.sogliaHum = ''
+        self.sogliaDis = ''
+        
+        if 'temp.txt' in os.listdir():
+            with open('temp.txt', 'r') as f:
+                self.sogliaTemp = f.read()
+        else:
+            with open('temp.txt', 'w') as f:
+                f.write(str(sogliaTemp))
+         
+        if 'hum.txt' in os.listdir():
+            with open('hum.txt', 'r') as f:
+                self.sogliaHum = f.read()
+        else:
+            with open('hum.txt', 'w') as f:
+                f.write(str(sogliaHum))
+                
+        if 'dis.txt' in os.listdir():
+            with open('dis.txt', 'r') as f:
+                self.sogliaDis = f.read()
+        else:
+            with open('dis.txt', 'w') as f:
+                f.write(str(sogliaDis))
+        
+        self.sogliaTemp = float(self.sogliaTemp)
+        self.sogliaHum = float(self.sogliaHum)
+        self.sogliaDis = float(self.sogliaDis)
         
         
     """ Legge i valori e ne crea un dizionario """
@@ -39,12 +65,18 @@ class SensorHandler:
         
     def setSogliaTemp(self, sogliaTemp):
         self.sogliaTemp = sogliaTemp
+        with open('temp.txt', 'w') as f:
+            f.write(str(sogliaTemp))
         
     def setSogliaHum(self, sogliaHum):
         self.sogliaHum = sogliaHum
+        with open('hum.txt', 'w') as f:
+            f.write(str(sogliaHum))
         
     def setSogliaDis(self, sogliaDis):
         self.sogliaDist = sogliaDist
+        with open('dis.txt', 'w') as f:
+            f.write(str(sogliaDis))
         
         
     def getSogliaTemp(self):
